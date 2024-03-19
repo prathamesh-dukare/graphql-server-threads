2950e65374cf5117b331e5cf000ecd25c4de0936
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2024-03-19 13:02:47 +0530

    add test mutations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import prismaClient from "./lib/db";
 const PORT = Number(process.env.PORT) || 4000;
 
 async function init() {
@@ -13,12 +14,42 @@ async function init() {
         hello: String,
         greet(nam:String): String
       }
+
+      type Mutation {
+        createUser( email:String, firstName: String, password: String): String
+      }
     `,
     resolvers: {
       Query: {
         hello: () => "Hello World",
         greet: (_, { nam }: { nam: String }) =>
           `Hello from the greet query ${nam}`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            email,
+            firstName,
+            password,
+          }: {
+            email: String;
+            firstName: String;
+            password: String;
+          }
+        ): Promise<String> => {
+          // Create user logic
+          await prismaClient.user.create({
+            data: {
+              email: email as string,
+              firstName: firstName as string,
+              password: password as string,
+              salt: "jdkef",
+            },
+          });
+
+          return "created ";
+        },
       },
     },
     // context: ({ req, res }) => ({ req, res }),
